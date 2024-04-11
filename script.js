1a83f738aefa89bc33eb3e3dc0037d635b8fb15a
Julian <jjvll90@gmail.com>
2024-04-10 22:26:53 -0500

    Add isError and cleanInputString function

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
+
+let isError = false;
+
+function cleanInputString(str) {
+    const strArray = str.split('');
+    let cleanStrArray = [];
+}
