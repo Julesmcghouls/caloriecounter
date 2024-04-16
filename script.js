aa9c21aab83ddb9cbe25398612b5a1f9ba132385
Julian <jjvll90@gmail.com>
2024-04-15 22:05:49 -0500

    Fix missing querySelectorAll argument

--- script.js
+++ script.js
@@ -20,5 +20,4 @@ return str.match(regex);
 function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
     const entryNumber = targetInputContainer.querySelectorAll();
-
     }
